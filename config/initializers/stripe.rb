90076f5dde931b77711577ba92871e8c0a89f001
Koki-Chance <kouki2525suddenly@gmail.com>
2020-10-04 14:56:15 +0900

    set_stripe.rb_in_initializers

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.configuration.stripe = {
+  publishable_key: ENV['STRIPE_PK'],
+  secret_key: ENV['STRIPE_SK']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
